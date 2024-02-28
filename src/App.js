225cd764f7aed6b5d3503abd89afc9ecfa972793
Vignesh <vignesh@Beamers-MacBook-Air.local>
2024-02-28 10:37:57 +0530

    id for hi bye buttons

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1> Hey there!</h1>
-      <button onClick={() => alert("hi")} id="hi button">
+      <button onClick={() => alert("hi")} id="hi-button">
         Say hi
       </button>
 
-      <button onClick={() => alert("bye")}> Say bye</button>
+      <button onClick={() => alert("bye")} id="bye-button">
+        {" "}
+        Say bye
+      </button>
     </div>
   );
 }
