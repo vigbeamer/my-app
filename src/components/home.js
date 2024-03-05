c59bdda7b94787f505b918c9a9b65253c1fa6066
Vignesh <vignesh@vig.local>
2024-03-05 15:20:18 +0530

    fix: remove beamer scripts

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,18 @@ import userflow from "userflow.js";
 
 const Home = () => {
   userflow.identifyAnonymous();
-  return <div>hi</div>;
+
+  return (
+    <div style={{ background: "blue", padding: 20, color: "white" }}>
+      Sed sed rhoncus quam. Duis tristique dapibus lacus, vitae efficitur metus
+      malesuada ac. Donec consectetur aliquam dolor, vel placerat magna
+      hendrerit at. Fusce quam dolor, imperdiet in mi vitae, tempus imperdiet
+      urna. Mauris ut dignissim ante. In hac habitasse platea dictumst. Donec
+      lobortis dictum nunc, eget dictum lectus imperdiet id. Cras mauris nunc,
+      bibendum et dignissim eu, ornare ut magna. Duis suscipit tempus odio.
+      Proin sit amet luctus massa.
+    </div>
+  );
 };
 
 export default Home;
