86fa6cfeb351a055ad3799627301838ec83af4ed
vigbeamer <vignesh.shanmugavelu@getbeamer.com>
2024-03-18 17:11:17 +0530

    feat: added dropdown

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,26 @@
 import userflow from "userflow.js";
 
 const Home = () => {
-  userflow.identifyAnonymous();
+  // userflow.identifyAnonymous();
 
   return (
-    <div style={{ background: "blue", padding: 20, color: "white" }}>
-      Sed sed rhoncus quam. Duis tristique dapibus lacus, vitae efficitur metus
-      malesuada ac. Donec consectetur aliquam dolor, vel placerat magna
-      hendrerit at. Fusce quam dolor, imperdiet in mi vitae, tempus imperdiet
-      urna. Mauris ut dignissim ante. In hac habitasse platea dictumst. Donec
-      lobortis dictum nunc, eget dictum lectus imperdiet id. Cras mauris nunc,
-      bibendum et dignissim eu, ornare ut magna. Duis suscipit tempus odio.
-      Proin sit amet luctus massa.
-    </div>
+    <>
+      <div style={{ background: "blue", padding: 20, color: "white" }}>
+        Sed sed rhoncus quam. Duis tristique dapibus lacus, vitae efficitur
+        metus malesuada ac. Donec consectetur aliquam dolor, vel placerat magna
+        hendrerit at. Fusce quam dolor, imperdiet in mi vitae, tempus imperdiet
+        urna. Mauris ut dignissim ante. In hac habitasse platea dictumst. Donec
+        lobortis dictum nunc, eget dictum lectus imperdiet id. Cras mauris nunc,
+        bibendum et dignissim eu, ornare ut magna. Duis suscipit tempus odio.
+        Proin sit amet luctus massa.
+      </div>
+
+      <select id="dropdwn">
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+    </>
   );
 };
 
